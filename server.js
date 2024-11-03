8d2aa08e615caeb305df81cd2d6e58bed1ebe8e2
iMythms <maythams.149@gmail.com>
2024-11-03 21:01:43 +0300

    Sign in done, sign up done

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const morgan = require('morgan')
 
+const session = require('express-session')
+
 const PORT = process.env.PORT ? process.env.PORT : '3000'
 
 mongoose.connect(process.env.MONGODB_URI)
@@ -16,9 +18,23 @@ mongoose.connection.on('connected', () => {
 	console.log(`Connected to MongoDB ${mongoose.connection.name}`)
 })
 
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 app.use(morgan('dev'))
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+	})
+)
+
+app.get('/', async (req, res) => {
+	res.render('index.ejs', {
+		user: req.session.user,
+	})
+})
 
 // Require and use Controller
 const authController = require('./controller/auth')
